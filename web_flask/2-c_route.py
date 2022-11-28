50ebcf3bcee50282c792db4d888256282078547b
FatjonMileti <tonimileti@yahoo.com>
2022-11-28 05:44:57 -0800

    ...

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,11 +9,13 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    ''' *** *** '''
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    ''' *** *** '''
     return 'HBNB'
 
 
