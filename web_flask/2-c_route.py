cc945b15dc256aa872c200ec7803dec5e18feb06
FatjonMileti <tonimileti@yahoo.com>
2022-11-28 05:50:42 -0800

    ...

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-'''
-Script for very basic web application. Returns the string
-requested by any device on the network depending on the phrases
-after the slash.
-'''
+''' *** *** '''
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    '''Returns the string below'''
-    return "Hello HBNB!"
+    ''' *** *** '''
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    '''Returns the string below'''
+    ''' *** *** '''
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_is(text):
-    ''' returns the string "C <text>".'''
+    ''' *** *** '''
     return f'C {text}'.replace('_', " ")
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
