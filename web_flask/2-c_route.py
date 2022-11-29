c24ddc09338e065d35864b4d311a875b46b922e4
FatjonMileti <tonimileti@yahoo.com>
2022-11-29 00:08:49 -0800

    ...

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-'''
-Script for very basic web application. Returns the string
-requested by any device on the network depending on the phrases
-after the slash.
-'''
+""" Starts a Flash Web Application C is FUN"""
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    '''Returns the string below'''
-    return "Hello HBNB!"
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    '''Returns the string below'''
+    """ Prints a Message when /hbnb is called """
     return 'HBNB'
 
 
-@app.route('/c/<text>')
-def c_is(text):
-    ''' returns the string "C <text>".'''
-    return f'C {text}'.replace('_', " ")
-
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', ' ')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
