c5b58a54f813ec1d33e06af6b42b64815ebc6230
FatjonMileti <tonimileti@yahoo.com>
2022-11-29 00:18:45 -0800

    ...

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-''' *** *** '''
-
-
+""" Starts a Flash Web Application Python is Cool"""
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    ''' *** *** '''
+    """ Prints a Message when / is called """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    ''' *** *** '''
+    """ Prints a Message when /hbnb is called """
     return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_is(text):
-    ''' *** *** '''
-    return f'C {text}'.replace('_', " ")
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', ' ')
+
 
 @app.route('/python', strict_slashes=False)
-@app.route('/python/<text>')
-def python(text='is cool'):
-    ''' *** *** '''
-    return f'Python {text}'.replace('_', " ")
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is_cool'):
+    """ Prints a Message when /python is called """
+    return "Python " + text.replace('_', ' ')
 
 if __name__ == "__main__":
     """ Main Function """
